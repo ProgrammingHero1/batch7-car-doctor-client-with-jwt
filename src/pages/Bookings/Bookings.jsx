cb2c6723092df55f51f82de06f7a1f1afe753bb0
Programming Hero <programming.hero1@gmail.com>
2023-05-10 11:26:16 -0500

    handle unauthorized access and forbidden access

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,10 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import { useNavigate } from "react-router-dom";
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
     const [bookings, setBookings] = useState([]);
+    const navigate = useNavigate();
 
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(() => {
@@ -15,8 +17,16 @@ const Bookings = () => {
             }
         })
             .then(res => res.json())
-            .then(data => setBookings(data))
-    }, [url]);
+            .then(data => {
+                if(!data.error){
+                    setBookings(data)
+                }
+                else{
+                    // logout and then navigate
+                    navigate('/');
+                }
+            })
+    }, [url, navigate]);
 
     const handleDelete = id => {
         const proceed = confirm('Are You sure you want to delete');
