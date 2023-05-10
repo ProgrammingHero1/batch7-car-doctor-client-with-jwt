faf0a1d10ff9ca68e4848d7cb1111bc678e08f33
Programming Hero <programming.hero1@gmail.com>
2023-05-10 10:39:22 -0500

    send jwt token to the server to verify

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -8,7 +8,12 @@ const Bookings = () => {
 
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(() => {
-        fetch(url)
+        fetch(url, {
+            method: 'GET', 
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('car-access-token')}`
+            }
+        })
             .then(res => res.json())
             .then(data => setBookings(data))
     }, [url]);
