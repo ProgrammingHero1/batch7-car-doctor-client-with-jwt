061111a56c96c7611280a15f0eab3d26b5d870d6
Programming Hero <programming.hero1@gmail.com>
2023-05-10 09:24:35 -0500

    basic auth redirect after login

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import { Navigate } from 'react-router';
+import {useLocation} from 'react-router-dom'
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
+    const location = useLocation()
 
     if(loading) {
         return <progress className="progress w-56"></progress>
@@ -13,7 +15,7 @@ const PrivateRoute = ({children}) => {
         return children;
     }
 
-    return <Navigate to="/login" replace></Navigate>;
+    return <Navigate to="/login" state={{from: location}} replace></Navigate>;
 };
 
 export default PrivateRoute;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
@@ -6,6 +6,10 @@ import { AuthContext } from '../../providers/AuthProvider';
 const Login = () => {
 
     const { signIn } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || '/';
 
     const handleLogin = event => {
         event.preventDefault();
@@ -17,6 +21,7 @@ const Login = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user);
+                navigate(from, { replace: true })
             })
             .catch(error => console.log(error));
     }
@@ -41,7 +46,7 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
